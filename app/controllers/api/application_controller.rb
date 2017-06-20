6b64d87b959b513c119ed3ebb1045e4437fdc1f2
Spencer Cheung <spencercheung1@gmail.com>
2017-06-20 16:11:23 -0700

    added api key search

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,4 +1,19 @@
 class Api::ApplicationController < ApplicationController
-  
+  # this will not throw an exception if an authenticity token is not passed.
+  # in other words, this will aloow post requests to be made
+  # from toolds other than a form rendered by rails
+  skip_before_action :verify_authenticity_token
 
+  def current_user
+    @user ||= User.find_by(api_key: params[:api_key]) unless params[:api_key].nil?
+  end
+
+  private
+  def api_key
+    # this grabs the api_key from the `Authorization` headers
+    # in the http request. It expects it to be in this format:
+    # "Apikey <the-api-key>"
+    match = request.headers['AUTHORIZATION'].match(/^Apikey (.+)/)
+    match.length == 2 ? match[1] : nil
+  end
 end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,10 +1,11 @@
-class Api::V1::QuestionsController < ApplicationController
+class Api::V1::QuestionsController < Api::ApplicationController
   skip_before_action :verify_authenticity_token
 
   before_action :find_question, only: [:show]
-
   def show
-    render json: @question
+    # test if we're getting the current_user by rendering it
+    render json: current_user
+    # render json: @question
   end
 
   def index
@@ -13,19 +14,19 @@ class Api::V1::QuestionsController < ApplicationController
     # after controller and it will render (in this case as json)
     # render json: @questions
 
-    # when using jBuilder, make sure that you do not render with 'render json:...'
-    # otherwise rails will serialize the model itself into json instead of using
-    # your jbuilder view
+    # when using jBuilder, make sure that you do not
+    # render with `render json: ...` otherwise rails will serialize
+    # the model itself into json instead of using your jbuilder view
     render
   end
 
   def create
-    return render json: params
     question = Question.new(question_params)
+
     if question.save
       render json: { id: question.id }
     else
-      render json: { errors: question.errors.full_messages}
+      render json: { errors: question.errors.full_messages }
     end
   end
 
