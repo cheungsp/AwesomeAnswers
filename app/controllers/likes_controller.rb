961153043cf60d0422e3a6e3d0d331139eb23532
Spencer Cheung <spencercheung1@gmail.com>
2017-06-26 15:06:40 -0700

    added ajax likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,33 @@
 class LikesController < ApplicationController
-before_action :authenticate_user!
-
-
+  before_action :authenticate_user!
   def create
     question = Question.find(params[:question_id])
-    like = Like.new(question: question, user: current_user)
+    @like = Like.new(question: question, user: current_user)
 
     if cannot? :like, question
-      flash[:alert] = "Can not like your own question"
-    elsif like.save
+      flash[:alert] = "Cannot like your own question, dummy!"
+    elsif @like.save
       flash[:notice] = "Thanks for liking!"
     else
-      flash[:alert] = like.pretty_errors
+      flash[:alert] = @like.pretty_errors
     end
-    redirect_to question_path(question)
+
+    @question = question
+    respond_to do |format|
+      format.html { redirect_to question_path(question) }
+      format.js { render }
+    end
   end
 
   def destroy
     like = Like.find(params[:id])
 
     if like.destroy
-      flash[:notice] = "😲"
+      flash[:notice] = "ð§"
     else
       flash[:alert] = like.pretty_errors
     end
 
     redirect_to question_path(like.question)
   end
-
 end
